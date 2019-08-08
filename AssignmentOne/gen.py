ae929922db1d2580a82de4bbacc0f071278ca3e4
Heath Logan Campbell <github@heathlogancampbell.com>
2019-08-09 09:18:24 +1200

    update readme

--- AssignmentOne/gen.py
+++ AssignmentOne/gen.py
@@ -1,3 +1,17 @@
+'''
+THIS PROGRAM WILL GENERATE A RANDOM INPUT FOR
+THE QUESTION OF ASSIGNMENT ONE. 
+
+YOU CAN RUN THE FILE WITH, WHICH WILL OUTPUT
+THE FILE TO ./test/sample-answer.txt
+RUN: Python ./gen.py > ./test/sample-answer.txt
+
+Height Width
+(0,0) (0,1) (0,2) .. (0, width)
+(1,0) (1,1) (1,2) .. (1, width)
+...
+(height,0) (height,1) (height,2) .. (height, width)
+'''
 import random
 
 height = 400
@@ -7,4 +21,5 @@ for y in range(height):
   line = []
   for x in range(width):
     line.append(str(random.randint(1,101)))
+  print(str(height), ' ', str(width))
   print(' '.join(line))
