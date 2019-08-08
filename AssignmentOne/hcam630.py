cf41bcaa80922d8fbfbd6e5e97b4f503ff441b09
Heath Logan Campbell <github@heathlogancampbell.com>
2019-08-09 01:50:03 +1200

    do a few values by hand to see the flow and increase map size

--- AssignmentOne/hcam630.py
+++ AssignmentOne/hcam630.py
@@ -8,10 +8,10 @@ import sys
 
 
 def isValid(map, pos):
-    height = len(map) + 1
-    width = len(map[0]) + 1
+    height = len(map)
+    width = len(map[0])
 
-    return (pos[0] <= width and pos[0] >= 0) and (pos[1] <= height and pos[1] >= 0)
+    return (pos[1] <= width and pos[1] >= 0) and (pos[0] <= height and pos[0] >= 0)
 
 
 '''
@@ -23,6 +23,7 @@ def isValid(map, pos):
 
 3,0    3,1    2,2
 '''
+
 
 '''
 every route has 2 path excluding diangals and edges
@@ -51,7 +52,7 @@ def getPaths(map, position):
 
     for nextPosition in positionsArray:
         if(isValid(map, nextPosition)):
-            possiblePaths.append((nextPosition, 1))
+            possiblePaths.append(nextPosition)
     return possiblePaths
 
 
@@ -78,8 +79,12 @@ while(True):
         break
     size = [int(s) for s in predecessor.split(' ')]
 
-    if(size[0] == size[1] == 0):
+    height = size[0]
+    width = size[1]
+    if(width == height == 0):
         break
+    height = height + 1
+    width = width + 1
 
     map = []
     for i in range(size[0]):
@@ -89,4 +94,47 @@ while(True):
         row = [int(s) for s in predecessor.split(' ')]
         map.append(row)
     bottomLeftChunk = (0, len(map) + 1)
-    dijkstra(map, bottomLeftChunk)
+
+    realMap = []
+    for i in range(height):
+        realMap.append([10000000000] * width)
+    startPos = (height - 1, 0)
+    visted = [startPos]
+    realMap[startPos[0]][startPos[1]] = 0
+
+    # Get all possible next paths
+    # We could move up
+    realMap[startPos[0] - 1][startPos[1]
+                             ] = map[startPos[0] - 1][startPos[1]]
+    # We could move up right
+    realMap[startPos[0] - 1][startPos[1] +
+                             1] = map[startPos[0] - 1][startPos[1] + 1 - 1]
+    #  We could move right
+    realMap[startPos[0]][startPos[1] +
+                         1] = map[startPos[0] - 1][startPos[1] - 1 + 1]
+
+    # round 2
+    currentWeight = realMap[startPos[0] - 1][startPos[1]]
+    realMap[startPos[0] - 1 - 1][startPos[1]] = currentWeight + map[startPos[0] - 1 - 1][startPos[1]]
+
+    realMap[startPos[0] - 1 - 1][startPos[1] + 1] = currentWeight + map[startPos[0] - 1 - 1][startPos[1]]
+
+    nextPos = currentWeight + map[startPos[0] - 1][startPos[1]+1]
+    if(realMap[startPos[0] - 1 - 1][startPos[1] + 1] > nextPos):
+      realMap[startPos[0] - 1 - 1][startPos[1] + 1] = map[startPos[0] - 1][startPos[1]+1]
+
+    # round 3
+    currentWeight = realMap[startPos[0] - 1][startPos[1] + 1]
+    nextPos = currentWeight + map[startPos[0] - 1 - 1][startPos[1] + 1]
+    if(realMap[startPos[0] - 1 - 1][startPos[1] + 1] > nextPos):
+      realMap[startPos[0] - 1 - 1][startPos[1] + 1] = nextPos
+
+    nextPos = currentWeight + map[startPos[0] - 1 - 1][startPos[1] + 1]
+    if(realMap[startPos[0] - 1 - 1][startPos[1] + 1 + 1] > nextPos):
+      realMap[startPos[0] - 1 - 1][startPos[1] + 1 + 1] = nextPos
+   
+      nextPos = currentWeight + map[startPos[0] - 1 - 1][startPos[1] + 1]
+    if(realMap[startPos[0] - 1][startPos[1] + 1 + 1] > nextPos):
+      realMap[startPos[0] - 1][startPos[1] + 1 + 1] = nextPos
+    
+    print(realMap)
