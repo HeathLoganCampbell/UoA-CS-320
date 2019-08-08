08680a8e62ab5dcbf6be0ba566a68679ecc731fe
Heath Logan Campbell <github@heathlogancampbell.com>
2019-08-09 08:12:30 +1200

    use heap instead of sorting

--- AssignmentOne/hcam630.py
+++ AssignmentOne/hcam630.py
@@ -33,49 +33,50 @@ while(True):
     realMap = []
     for i in range(height):
         realMap.append([10000000000] * width)
-    startPos = (height - 1, 0)
+    startPos = (0,height - 1, 0)
     visted = [startPos]
-    realMap[startPos[0]][startPos[1]] = 0
+    realMap[startPos[1]][startPos[2]] = startPos[0]
 
     # Get all possible next paths
     # We could move up
 
     seen = []
-    
+    heapq.heapify(seen) 
+
     currentPos = startPos
     while(True):
       for pos in [(-1,0,0,1), (-1, 1, 0, 1), (0, 1, 0, 1),   (1, 1, 0, 1), (1, 0, 0, 1), (1, -1, 0, 1), (0, -1, 0, 1), (-1, -1, 0, 1)]:
-        vertY = currentPos[0] + pos[0]
-        vertX = currentPos[1] + pos[1]
+        vertY = currentPos[1] + pos[0]
+        vertX = currentPos[2] + pos[1]
         if(vertY < 0 or vertY >= len(realMap)):
           continue
         if(vertX < 0 or vertX >= len(realMap[0])):
           continue
         vertixLoc = realMap[vertY][vertX]
 
-        weightY = currentPos[0] + pos[2] - 1
-        weightX = currentPos[1] + pos[3] - 1
+        weightY = currentPos[1] + pos[2] - 1
+        weightX = currentPos[2] + pos[3] - 1
         if(weightY < 0 or weightY >= len(map)):
           continue
         if(weightX < 0 or weightX >= len(map[0])):
           continue
         
-        if(currentPos[0] < 0 or currentPos[0] >= len(realMap)):
+        if(currentPos[1] < 0 or currentPos[1] >= len(realMap)):
           continue
-        if(currentPos[1] < 0 or currentPos[1] >= len(realMap[0])):
+        if(currentPos[2] < 0 or currentPos[2] >= len(realMap[0])):
           continue
         
 
-        weights = realMap[currentPos[0]][currentPos[1]] + map[weightY][weightX]
+        weights = realMap[currentPos[1]][currentPos[2]] + map[weightY][weightX]
         if(vertixLoc > weights):
-          realMap[currentPos[0] + pos[0]][currentPos[1] + pos[1]] = weights
-          seen.append((currentPos[0] + pos[0], currentPos[1] + pos[1], weights))
+          realMap[currentPos[1] + pos[0]][currentPos[2] + pos[1]] = weights
+          heapq.heappush(seen, (weights, currentPos[1] + pos[0], currentPos[2] + pos[1])) 
           # Might be wrong?
-          if(currentPos[0] + pos[0] == 0 and currentPos[1] + pos[1] == width - 1 ):
+          if(currentPos[1] + pos[0] == 0 and currentPos[2] + pos[1] == width - 1 ):
             break
-      seen.sort(key=lambda tup: tup[2])
+     
       # print(seen)
-      currentPos = seen.pop(0)
+      currentPos = heapq.heappop(seen)
       if(len(seen) == 0):
         break
 
